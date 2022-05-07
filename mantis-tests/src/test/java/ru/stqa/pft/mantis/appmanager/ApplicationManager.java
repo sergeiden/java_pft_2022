1e4eabfbef435b473cab4ce8fcd26aefc82ec408
sergeiden <sd100578@yandex.ru>
2022-05-07 19:50:29 +0300

    Добавлен сценарий Login по протоколу HTTP

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
 
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Properties;
@@ -15,6 +16,7 @@ public class ApplicationManager {
 
   private final Properties properties;
   WebDriver wd;
+
   private String browser;
 
   public ApplicationManager(String browser) {
@@ -23,22 +25,30 @@ public class ApplicationManager {
   }
 
   public void init() throws IOException {
-  String target = properties.getProperty("target", "local");
+    String target = properties.getProperty("target", "local");
 
-    properties.load(new FileReader(String.format("src/test/resources/%s.properties", target)));
+    properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
 
     if (browser.equals(BrowserType.FIREFOX)) {
       wd = new FirefoxDriver();
-    } else if (browser.equals (BrowserType.CHROME)) {
+    } else if (browser.equals(BrowserType.CHROME)) {
       wd = new ChromeDriver();
     } else if (browser.equals(BrowserType.IE)) {
       wd = new InternetExplorerDriver();
     }
     wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
     wd.get(properties.getProperty("web.baseUrl"));
-    }
+  }
 
   public void stop() {
     wd.quit();
+  }
+
+  public HttpSession newSession(){
+    return new HttpSession (this);
+  }
+
+  public String getProperty(String key) {
+    return properties.getProperty(key);
   }
 }
